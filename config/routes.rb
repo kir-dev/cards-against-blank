b79bdef4dbb2bf9b690729f79784d7a91b1adc79
norbert <schauernorbert@hotmail.com>
2019-05-14 01:29:50 +0200

    devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :rooms
   resources :players
-  resources :users
   resources :decks
   resources :black_cards
   resources :white_cards
+  root 'white_cards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
